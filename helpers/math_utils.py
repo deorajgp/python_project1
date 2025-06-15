6e7ccb388b73ad1e551b1be28dd3c825ccbfcf92
Deoraj Gope <deoraj.gope@jktech.com>
2025-06-15 17:12:38 +0530

    add addition and multiplication functions

---
+++ helpers/math_utils.py
@@ -0,0 +1,18 @@
+import math.prod
+
+"""
+
+helper functions to perform on the array of integers
+
+"""
+
+
+
+# function to perform addition of all the elements and return the sum
+def add(nums:List[int])->int:
+    return sum(nums)
+
+
+#function to perform multiplication of all the elements and return the result
+def mul(nums:List[int])->int:
+    return prod(nums)
