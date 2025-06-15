8305fd2e865074321c70502b6d76196d3ead4fd9
Deoraj Gope <deoraj.gope@jktech.com>
2025-06-15 17:11:59 +0530

    add parity checker function

---
+++ helpers/general_utils.py
@@ -0,0 +1,12 @@
+"""
+
+general utility functions 
+
+"""
+
+# returns parity of a function func after calling it on a list
+# if the result of calling the function is even parity is 1 else 0
+# funciton assumes that the list of integers is provided with num of elements greater than 0
+def parity_checker(func , nums:List[int])->bool:
+    result = func(nums)
+    return result % 2 == 0
