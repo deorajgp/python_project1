121161f0cc1807bb2a715f61a74a864e7a87814d
white Rabbit <deoraj.gope@jktech.com>
2025-07-09 09:01:00 +0000

    small fixes

--- helpers/math_utils.py
+++ helpers/math_utils.py
@@ -1,4 +1,3 @@
-from typing import List
 import math
 
 """
@@ -10,16 +9,16 @@ helper functions to perform on the array of integers
 
 
 # function to perform addition of all the elements and return the sum
-def add(nums:List[int])->int:
+def add(nums:list[int])->int:
     return sum(nums)
 
 
 #function to perform multiplication of all the elements and return the result
-def mul(nums:List[int])->int:
+def mul(nums:list[int])->int:
     return math.prod(nums)
 
 #function to perform division of first number with last number
-def div(nums:List[int])->int:
+def div(nums:list[int])->int:
     if nums[-1] == 0:
         raise ValueError("Cant divide by zero as last number is zero")
     
