adb9e856dac468d7cedf4732c2bbf96a54980eaa
white Rabbit <deoraj.gope@jktech.com>
2025-07-09 09:00:30 +0000

    fix part where the program after taking full input again runs the whole loop to take numbers as input

--- helpers/general_utils.py
+++ helpers/general_utils.py
@@ -1,6 +1,3 @@
-from typing import List
-import time
-
 """
 
 general utility functions 
@@ -10,20 +7,20 @@ general utility functions
 # returns parity of a function func after calling it on a list
 # if the result of calling the function is even parity is 1 else 0
 # funciton assumes that the list of integers is provided with num of elements greater than 0
-def parity_checker(func , nums:List[int])->List[int]:
+def parity_checker(func , nums:list[int])->list[int]:
     result = func(nums)
     return [result % 2 == 0 , result]
 
 
 #Take input from user the last number cant be zero
-def take_input_of_n_integers(nums:List[int],mn_size = 1):
+def take_input_of_n_integers(nums:list[int],mn_size = 1):
     while True:
         try:
             size = 0
             while size<mn_size:
                 size = input("Enter the size of list ")
                 size = int(size)
-                if size<mn_size:
+                if size<2:
                     print("Atleast size 2 must be provided")
                     continue
                 else:
@@ -31,18 +28,23 @@ def take_input_of_n_integers(nums:List[int],mn_size = 1):
 
             #enter the elements of nums
             while len(nums)!=size:
-                ele = input(f"Enter element number {len(nums) + 1} ")
-                ele = int(ele)
-                nums.append(ele)
+                try:
+                    ele = input(f"Enter element number {len(nums) + 1} ")
+                    ele = int(ele)
+                    nums.append(ele)
+                except ValueError as e:
+                    print("Please enter an integer ")
 
-            #This part uses sleep function from time module in python for enhanced user experience       
-            #https://www.programiz.com/python-programming/time/sleep
             while nums[-1]==0:
                 print("The last number cannot be zero so plzz enter something different")
-                time.sleep(2)
-                ele = input("Enter the last number")
-                ele = int(ele)
-                nums.append(ele)
-                
+                try:
+                    ele = input("Enter the last number")
+                    ele = int(ele)
+                    nums[-1] = ele
+                except ValueError:
+                    print("Please enter an integer ")
+            
+            break
+
         except ValueError as e:
-                raise e
+                print(e)
